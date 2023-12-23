052a676aab6fcf21397d2f20fdeeb1eb1a723ad9
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-23 01:22:41 -0300

    feat: now we can create user in db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/users', async (req, reply) => {
+  const regiisterBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = regiisterBodySchema.parse(req.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
+});
+
+// const prisma = new PrismaClient();
