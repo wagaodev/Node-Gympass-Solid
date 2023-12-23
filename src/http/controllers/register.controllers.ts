e555da466e2053cac00083cc560bb686c891b6e0
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2023-12-23 01:39:55 -0300

    refactor: abstract the responsability

---
+++ src/http/controllers/register.controllers.ts
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+export async function registerController(
+  req: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(req.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+
+  return reply.status(201).send();
+}
